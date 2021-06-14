e85eff7d6b7527ac7f6caaa069c01682875c1381
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-14 12:19:01 +0800

    📝 Use Userbox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from 'src/styles/Home.module.css'
+import { UserBox } from "src/components/UserBox"
 
 export default function Home() {
   return (
@@ -14,39 +15,14 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Next.js Data Fetching SSR Test</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <UserBox
+            name="Name"
+            username="username"
+            email="email"
+          />
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
@@ -62,6 +38,7 @@ export default function Home() {
           </span>
         </a>
       </footer>
+
     </div>
   )
 }
