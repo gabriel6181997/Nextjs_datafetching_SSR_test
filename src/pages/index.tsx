7f0319145943d3f91e2012a8ddac56528e23ed60
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-14 15:31:34 +0800

    🔥 Remove redundant import code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from 'src/styles/Home.module.css'
 import { UserBox } from "src/components/UserBox"
 import Link from "next/link";
