89569e73e5f11c2bb22e43dc4ff1f9138ebaa73b
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-14 15:34:37 +0800

    💄 Add UI of Personal Information page

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,6 +1,13 @@
 const UserInfo = () => {
    return(
-     <div>HELLO!</div>
+     <div>
+       <h1>Name 's Personal Information</h1>
+       <p>Email : email</p>
+       <p>Username : username</p>
+       <p>id : id</p>
+       <p>phone : phone</p>
+       <p>website : website</p>
+     </div>
    )
 }
 
