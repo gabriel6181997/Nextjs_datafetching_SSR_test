96f12cf4e5c1ac9479f95ffaff42310f9b129935
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-14 18:02:21 +0800

    ✨ Use getStaticProps and getStaticPaths

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,12 +1,33 @@
-const UserInfo = () => {
+import { getAllUserIds, getUserData } from "src/util/getFunctions";
+
+export async function getStaticProps({params}){
+  const {data} = await getUserData(params.id);
+  return {
+    props: {
+      userData: data,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = await getAllUserIds();
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+const UserInfo = ( {userData}) => {
+  console.log(userData);
+
    return(
      <div>
-       <h1>Name 's Personal Information</h1>
-       <p>Email : email</p>
-       <p>Username : username</p>
-       <p>id : id</p>
-       <p>phone : phone</p>
-       <p>website : website</p>
+       <h1>{userData.name}'s Personal Information</h1>
+       <p>Email : {userData.email}</p>
+       <p>Username : {userData.username}</p>
+       <p>id : {userData.id}</p>
+       <p>phone : {userData.phone}</p>
+       <p>website : {userData.website}</p>
      </div>
    )
 }
