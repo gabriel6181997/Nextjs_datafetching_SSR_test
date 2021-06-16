f2db8254169db44bb1934eecaa830c2a0aa84c6b
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-16 11:58:03 +0800

    🎨 Change to arrow function

--- src/util/getFunctions.tsx
+++ src/util/getFunctions.tsx
@@ -1,8 +1,10 @@
-export async function getAllUserIds() {
+import { UserInfoType } from "./type";
+
+export  const getAllUserIds = async () =>  {
    const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
    const data : UserInfoType[] = await res.json();
 
-   return data.map((user: any) => {
+   return data.map((user) => {
      return {
         params : {
           id: user.id.toString(),
@@ -11,7 +13,7 @@ export async function getAllUserIds() {
    });
 }
 
-export async function getUserData(id: string) {
+export const getUserData = async (id: string) => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
   const data = await res.json();
   return {
